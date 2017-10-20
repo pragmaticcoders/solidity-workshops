3b0bfc295c7e0688f7f9e2c040ff5a63f9e992b2
Marcin Biernat <marcin.biernat@pragmaticcoders.com>
2017-10-20 16:10:37 +0200

    Prepare to Kovan / IPFS deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,11 +2,7 @@ const EtherRoulette = artifacts.require("./EtherRoulette.sol");
 const Random = artifacts.require("./Random.sol");
 
 async function deploy(deployer) {
-  const MAX_NUMBER = 6;
-  const INITIAL_VALUE = web3.toWei(5, 'ether');
-
   await deployer.deploy(Random);
-  await deployer.deploy(EtherRoulette, Random.address, MAX_NUMBER, {value: INITIAL_VALUE});
 }
 
 module.exports = function (deployer) {
--- truffle.js
+++ truffle.js
@@ -1,9 +1,22 @@
+const Web3 = require('web3');
+
+
+const gasPrice = new Web3().toWei(10, 'gwei');
+
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*",
+      gasPrice,
+    },
+    kovan: {
+      host: "localhost",
+      port: 8546,
+      network_id: "42",
+      gasPrice,
     }
   }
 };
